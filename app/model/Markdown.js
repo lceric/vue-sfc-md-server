eb1c8efb505613f7cae2a18e28a1443e92a1fde1
Eric <lichao@kaike.la>
2021-01-19 14:34:56 +0800

    update vue-sfc-md

--- app/model/Markdown.js
+++ app/model/Markdown.js
@@ -4,13 +4,19 @@ const to = require('await-to-js').default
 class Markdown {
   async parsed(sourceStr) {
     if (!sourceStr) return [null, '']
-    let [err, doc] = await to(parsed(sourceStr))
+    let [err, doc] = await to(
+      parsed(sourceStr, {
+        jsx: true,
+        removeEmptyColumns: true,
+        emptyFlags: ['-', '`false`'],
+      })
+    )
     if (err) {
       return [err]
     }
     let sfcDoc = doc
+      .filter((m) => !m.empty)
       .map((m) => m.doc)
-      .filter((itm) => !itm.empty)
       .join('\n')
     return [null, sfcDoc]
   }
